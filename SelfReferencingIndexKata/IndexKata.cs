4c8e41bcd3be27302e38e8d2ffb888e5c19bfeac
oleksiyo <kozzzak@gmail.com>
2013-01-27 13:45:51 +0200

    first commit

---
+++ SelfReferencingIndexKata/IndexKata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfReferencingIndexKata
+{
+    public class IndexKata
+    {
+       public int[] Index(string input)
+        {
+            return null;
+        }
+    }
+    public class IndexKataTests
+    {
+ 
+    }
+}
